ce6eaee50aae374b68b10a49012edd58a35ec6d1
Abhinav <abhinav@ulabs.io>
2018-06-06 18:30:57 -0400

    Create divisions from db

--- src/js/scripts/grid.js
+++ src/js/scripts/grid.js
@@ -3,15 +3,16 @@ var Grid = {
   removeSelectedBorders: function() {
     $(".ui-selected").css("border", "none");
   },
-  createRoomFromDb: function(roomData) {
-    var bldg = $("[data-building='" + roomData.buildingId + "']");
-    var floor = bldg.find("[data-floor='" + roomData.floorId + "']");
-    var roomCoordinates = roomData.coordinates;
-    roomCoordinates.forEach(function(roomPart, i) {
-      var rows = parseInt(roomPart.x3 - roomPart.x0);
-      var cols = parseInt(roomPart.y3 - roomPart.y0);
-      for (var i = roomPart.x0; i <= roomPart.x3; i++) {
-        for (var j = roomPart.y0; j <= roomPart.y3; j++) {
+
+  createBlockFromDb: function(blockData) {
+    var bldg = $("[data-building='" + blockData.buildingId + "']");
+    var floor = bldg.find("[data-floor='" + blockData.floorId + "']");
+    var blockCoordinates = blockData.coordinates;
+    blockCoordinates.forEach(function(blockPart, i) {
+      var rows = parseInt(blockPart.x3 - blockPart.x0);
+      var cols = parseInt(blockPart.y3 - blockPart.y0);
+      for (var i = blockPart.x0; i <= blockPart.x3; i++) {
+        for (var j = blockPart.y0; j <= blockPart.y3; j++) {
           floor.find("[data-tile='" + i + "x" + j + "']").css({
             border: "none"
           });
@@ -19,7 +20,13 @@ var Grid = {
       }
     });
   },
+  createRoomFromDb: function(roomData) {
+    Grid.createBlockFromDb(roomData);
+  },
 
+  createDivisionFromDb: function(divisionData) {
+    Grid.createBlockFromDb(divisionData);
+  },
   createBlockFromSelection: function() {
     var newBlock = {};
     var newBlockCoordinates = {
@@ -75,49 +82,6 @@ var Grid = {
       .write();
   },
   createRoomFromSelection: function() {
-    // var newRoom = {};
-    // var newRoomCoordinates = {
-    //   x0: 0,
-    //   y0: 0,
-    //   x3: 0,
-    //   y3: 0
-    // };
-
-    // var buildingId = null;
-    // var floorId = null;
-
-    // var selectedRoomTiles = [];
-    // // Get coordinates of selected including floor and building
-    // $(".ui-selected")
-    //   .filter(function() {
-    //     // Check if its a row and filter it out
-    //     var attr = $(this).attr("data-row");
-    //     return typeof attr === typeof undefined || attr === false;
-    //   })
-    //   .each(function() {
-    //     var tile = $(this);
-    //     var tileLocation = JSON.parse(tile.attr("data-location"));
-    //     // console.log(tileLocation);
-    //     selectedRoomTiles.push({ x: tileLocation.x, y: tileLocation.y });
-    //     if (buildingId === null) {
-    //       buildingId = tileLocation.buildingId;
-    //     }
-    //     if (floorId === null) {
-    //       floorId = tileLocation.floorId;
-    //     }
-    //   })
-    //   .css("border", "none");
-
-    // var tileCount = selectedRoomTiles.length;
-    // newRoomCoordinates.x0 = selectedRoomTiles[0].x;
-    // newRoomCoordinates.y0 = selectedRoomTiles[0].y;
-    // newRoomCoordinates.x3 = selectedRoomTiles[tileCount - 1].x;
-    // newRoomCoordinates.y3 = selectedRoomTiles[tileCount - 1].y;
-
-    // newRoom.buildingId = buildingId;
-    // newRoom.floorId = floorId;
-    // newRoom.coordinates = [newRoomCoordinates];
-
     var newRoom = Object.assign({}, Grid.createBlockFromSelection());
 
     var roomName = prompt("Enter unique room name: ");
@@ -233,6 +197,20 @@ var Grid = {
         for (var room = 0; room < rooms.value().length; room++) {
           var roomData = rooms.value()[room];
           Grid.createRoomFromDb(roomData);
+        }
+
+        // Get divisons in the floor on this building
+        var divisions = DB.db
+          .get("divisions")
+          .filter({ buildingId: building.id, floorId: floor.id });
+
+        for (
+          var division = 0;
+          division < divisions.value().length;
+          division++
+        ) {
+          var divisionData = divisions.value()[division];
+          Grid.createDivisionFromDb(divisionData);
         }
       }
 
