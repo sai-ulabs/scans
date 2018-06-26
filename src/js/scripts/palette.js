f4577d5d24fc531cc5ca2a21fac5215542045f8e
Abhinav <abhiulabs@gmail.com>
2018-06-26 19:40:34 -0400

    update right bar

--- src/js/scripts/palette.js
+++ src/js/scripts/palette.js
@@ -1,12 +1,12 @@
-$(".palette button#btnCreateRoom").on("click", function(e) {
+$("button#btnCreateRoom").on("click", function(e) {
   Grid.createRoomFromSelection();
 });
 
-$(".palette button#btnRemoveBorders").on("click", function(e) {
+$("button#btnRemoveBorders").on("click", function(e) {
   Grid.createDivisionFromSelection();
 });
 
-$(".palette input#hasImageCheck").on("click", function(e) {
+$("input#hasImageCheck").on("click", function(e) {
   function recreate() {
     Grid.init();
     DragDrop.init();
